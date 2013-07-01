d1e7c5395d4bf628c424b99adfb522d5af50d6b0
Adam Derewecki <derewecki@gmail.com>
2013-06-30 22:44:07 -0700

    Add timing information

--- tpb.rb
+++ tpb.rb
@@ -5,12 +5,13 @@ require 'open-uri'
 
 # http://thepiratebay.sx/search/waking%20life/0/7/e
 q = URI::encode(ARGV[0..-1].join(' '))
-puts "q: '#{ q }'"
 search_results_url = "http://thepiratebay.sx/search/#{ q }/0/7/0"
-puts "* #{ search_results_url }"
+search_results = open(search_results_url)
 
-if false
-  doc = Nokogiri::HTML(open(search_results_url))
+t = Time.now
+if ENV['xml']
+  puts 'XML'
+  doc = Nokogiri::HTML(search_results)
 
   # first img in the #searchResult table is the magnet link; grab parent
   magnet_link = doc.at('#searchResult tr img').parent()['href']
@@ -18,7 +19,8 @@ if false
   puts "#{ seeds } seeds"
   puts magnet_link
 else
-  doc = open(search_results_url).read
+  puts 'Regex'
+  doc = search_results.read
   magnet_links = doc.scan(/"(magnet:.*?)"/)
 
   seeds_and_leechers = doc.scan(/td align="right">(\d+)</)
@@ -28,3 +30,5 @@ else
   magnet_links_and_seeds = magnet_links.zip(seeds)
   puts magnet_links_and_seeds.first
 end
+elapsed = Time.now - t
+puts "Elapsed: #{ elapsed }"
